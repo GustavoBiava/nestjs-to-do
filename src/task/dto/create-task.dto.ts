aba04f513dbb972e0cd8195668e221d10c12acd3
GustavoBiava <gustavo.rbiava@hotmail.com>
2025-03-04 18:07:08 -0300

    Columns validations

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, Length } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString({ message: 'Title must be a string!' })
+  @Length(3, 25, { message: 'Title invalid lenght! (Min: 3, Max: 25)' })
   public title: string;
+
+  @IsString({ message: 'Description must be a string!' })
+  @Length(3, 50, { message: 'Description invalid lenght! (Min: 3, Max: 50)' })
   public description: string;
 }
--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { IsNumber } from 'class-validator';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsNumber({}, { message: 'Status must be a number!' })
   public status?: number;
 }
